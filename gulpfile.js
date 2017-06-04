abc798833bbb13b425849917ec74616b127dfdf2
j-r <johnreardon@Johns-MacBook-Air-2.local>
2017-06-04 16:13:42 -0500

    Cafe Website Template v1.1
    Version 1.1 of a basic one page website I made for an imaginary cafe.
    The website has a responsive design and features a “About” section,
    “Menu” section, and “Contact” section.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+var gulp = require('gulp');
+var image = require('gulp-image');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+
+gulp.task('compressImages', () => {
+		gulp.src('./prod/*')
+		.pipe(image({
+			mozjpeg: false,
+			jpegoptim: false,
+			pngquant: true,
+			optipng: false,
+			zopflipng: true,
+			jpegRecompress: true,
+			gifsicle: true,
+			svgo: true
+		}))
+		.pipe(gulp.dest('./imgs/'));
+});
+
+gulp.task('process-css', () => {
+	return gulp.src(['css/application.css'])
+		.pipe(cleanCSS())
+		.pipe(concat('prod.min.css'))
+		.pipe(gulp.dest('./dist'));
+});
